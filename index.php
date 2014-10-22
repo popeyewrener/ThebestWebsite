5209ccdcd74542d524603e6c4f38a421ae698c78
kamolchai <yakamigolf@gmail.com>
2014-10-22 12:05:30 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include 'body/title.php'; ?>
+<script type="text/javascript" src="./js/jquery.spritely-0.1.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#balloon')
+		.sprite({ fps: 3, no_of_frames: 2 })
+		.spRandom({
+		    top: 320,
+		    left: 400,
+		    right: 1300,
+		    bottom: 150,
+		    speed: 8500,
+		    pause: 1000
+		});
+    });
+</script>   
+    
+</head>
+<body>
+<?php include 'body/topmenu.php';?>
+<div id="main_part">
+    <div id="balloon" style="top: 157px; left: 1000px; background-position: -82px 0px;">
+    </div>
+  <div id="main_part_in">
+    <h2>THE BEST
+        </br>BESTTER TOGETHER</h2>
+  </div>
+</div>
+
+<div id="content">
+  <div class="thirds">
+    <h3>บริษัทเดอะเบสท์ทำอะไร</h3>
+    <p>เราคือบริษัทขายตรงโดยใช้แผนธุรกิจแบบเครืือข่าย(MLM) เรามีสาขาอยู่ทั่วอาเซียน</p>
+  </div>
+  <div class="thirds">
+    <h3>ทำไมต้อง? TheBestInternational</h3>
+    <p>บริษัทเติบโตอย่างรวดเร็วทั้งในไทยและต่างประเทศ อีกทั้งสมาชิกยังได้รับผลประโยชน์สูง</p>
+  </div>
+  <div class="thirds">
+    <h3>สนใจอยากร่วมงาน ต้องทำอย่างไรบ้าง?</h3>
+    <p>ง่ายๆ เพียงแค่คุณโทรมาเบอร์นี้ 0817164420 หรือ ติดต่อทางเว็บก็ได้</p>
+  </div>
+  <hr class="cleanit">
+  <div class="banner1">
+    <p>เอกสารการสมัครสมาชิก</p>
+    <p> <a href="http://www.thebestnano.com" class="button_dark">ดาวน์โหลด</a> </p>
+  </div>
+  <h3>ประกาศ!! ข่าวและโปรโมชั่น</h3>
+  <div class="fourths_portfolio">
+    <h4>Promotion ใจดีแจกทอง<br>
+      <span>คลิ๊กเพื่ออ่านต่อ</span> </h4>
+    <a href="#"><img src="img/portfolio1.jpg" alt=""></a> </div>
+  <div class="fourths_portfolio">
+    <h4>Promotion เที่ยวเกาหลี<br>
+      <span>คลิ๊กเพื่ออ่านต่อ</span> </h4>
+    <a href="#"><img src="img/portfolio2.jpg" alt=""></a> </div>
+  <div class="fourths_portfolio">
+    <h4>Promotion เดือนกันยายน<br>
+      <span>คลิ๊กเพื่ออ่านต่อ</span> </h4>
+    <a href="#"><img src="img/portfolio3.jpg" alt=""></a> </div>
+  <div class="fourths_portfolio">
+    <h4>งานABC NIGHT<br>
+      <span>คลิ๊กเพื่ออ่านต่อ</span> </h4>
+    <a href="#"><img src="img/portfolio4.jpg" alt=""></a> </div>
+  <hr class="cleanit">
+  <p class="youlike">ถ้าท่านต้องการดูทั้งหมด <a href="news.php">คลิ๊กที่นี่</a> </p>
+  <div class="cara"></div>
+  <div class="about_blok">
+    <h3>อย. ผลิตภัณของเรา และ ใบสคบ.</h3>
+    <p class="about">เราได้รับใบรับประกัน ว่าเป็นบริษัทที่เปิดอย่างถูกต้องตามกฏหมายและผลิตภัณมีคุณภาพ</p>
+    <a href="skb.php" class="button_light">ข้อมูลเพิ่มเติม</a> </div>
+    <section class="ha-waypoint" data-animate-down="ha-header-show" data-animate-up="ha-header-hide">
+  <div class="testimonials">
+    <div class="quote">วิสัยทัศน์ 
+         บริษัทเป็นหนึ่งในธุรกิจเครือข่ายที่ได้รับการยอมรับมากที่สุด เป็นที่รู้จักในฐานะผู้สร้างแรงบันดาลใจ หล่อหลอมจิตวิญญาณ พัฒนาและเปลี่ยนแปลงคุณภาพชีวิต ของผู้คนโดยทำให้คนเป็นคนสร้างตน และสร้างคนที่มีคุณภาพ และประสบความสำเร็จได้มากขึ้นกว่าที่เป็นอยู่ในปัจจุบัน</div>
+    <div class="quote">พันธะกิจ 
+          มุ่งมั่นพัฒนาองค์กร ผลิตภัณฑ์ที่มีคุณภาพ มีระบบพัฒนาคนที่มีประสิทธิผล เพื่อเป็นโอกาสในการสร้างสรรค์ชีวิตให้ประสบความสำเร็จได้อย่างยั่งยืน ทั้งในทางการยกระดับความรู้ความสามารถ ความสุขฐานะทางการเงิน และฐานะทางสังคม</div>
+    <div class="quote">เราจะเป็นบริษัท ที่จะอยู่ร่วมกันกับสมาชิก
+โดยให้ความสำคัญกับค่านิยม 8 ประการดังนี้
+1. ซื่อสัตย์และไว้วางใจได้
+2. เปิดใจยอมรับ ให้เกียรติผู้คน
+3. มีความรักและความสนุกกับงานที่ทำ
+4. เรียนรู้และพัฒนา งานเองอย่างต่อเนื่อง
+5. เจริญเติบโตเพื่อความสำเร็จที่ยั่งยืน
+6. เรียบง่ายไม่ซับซ้อน
+7. ใส่ใจในความสุขกายและสุขใจ
+8. ยึดถือ คุณภาพ คุณธรรม และความพอใจเป็นสำคัญ</div>
+    <span class="testimonials_bottom"></span>
+    <ul class="controls">
+      <li><a href="#"></a></li>
+      <li><a href="#"></a></li>
+      <li><a href="#"></a></li>
+    </ul>
+    <span class="by">ข่าวด่วนวันนี้!</span> </div></section>
+</div>
+
+<?php include 'body/footer.php'; ?>
+</body>
+</html>
